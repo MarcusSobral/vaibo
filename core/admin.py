3bbb10d9cba9db53275c31484d5e0ecebe1a3aa8
MarcusSobral <marcusvinicius14@gmail.com>
2021-05-17 23:41:05 -0300

    Criado model Smartphone 2

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Smartphone
+
+
+@admin.register(Smartphone)
+class SmartphoneAdmin(admin.ModelAdmin):
+    list_display = ('release_date', 'name', 'maker', 'display_size_pixels', 'storage', 'camera_pixels', 'chipset_short', 'batery_size')
