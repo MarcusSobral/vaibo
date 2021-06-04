7790752264400335db120aae31b9279a4094d63c
MarcusSobral <marcusvinicius14@gmail.com>
2021-06-04 01:43:23 -0300

    Carga tabela Produtos e integração de notas com gráficos

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from .models import Smartphone
+from .models import Produtos
 
 
 class IndexView(TemplateView):
@@ -8,5 +8,16 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['smartphones'] = Smartphone.objects.all()
+        context['produtos'] = Produtos.objects.all()
         return context
+
+# from .models import Smartphone
+
+
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(IndexView, self).get_context_data(**kwargs)
+#         context['smartphones'] = Smartphone.objects.all()
+#         return context
